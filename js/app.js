9df725f051713a9eb997203ab5f056c06651d451
billal-fahmid <billalcooom@gmail.com>
2023-03-02 23:46:55 +0600

    feature , name and date dynamicaly load the card

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const loadData = () => {
 
 const displayAllData = (data) => {
     const cardContainer = document.getElementById('card-container');
-    data.forEach(singleData => {
+    data.slice(0,6).forEach(singleData => {
         console.log(singleData)
         const {image,features,name,published_in}=singleData;
         
@@ -15,22 +15,30 @@ const displayAllData = (data) => {
         // const ol = document.createElement('ol');
         cardContainer.innerHTML += `  
         <div class="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure class="py-10 px-10"><img class="rounded-lg" src="${image}" alt="Shoes" /></figure>
+        <figure class="py-10 px-10"><img class="rounded-lg h-48" src="${image}" alt="Shoes" /></figure>
         <div class="card-body">
-            <h2 class="card-title">Features</h2>
-           <ol>
-           ${features.map(feature=>{
-            return `<li> ${feature}</li>`
-            
-         })}
+         
+           <h2 class="card-title text-[#111111]">Features</h2>
+
+           <ol class="list-decimal pl-4">
+           ${features.map(feature=>(`<li class="text-[16px] font-semibold text-[#585858]">${feature}</li>`))}
            </ol>
-            
-            <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+
+           <hr>
+            <div class="card-actions flex justify-between items-center h-20 ">
+                <div>
+                <h2 class="card-title text-[#111111]">${name}</h2>
+                <h3 class="text-[#585858]">${published_in}</h3>
+                </div>
+                <div class="">
+                
+                <button class="btn btn-circle border-none bg-orange-300 hover:bg-orange-200">
+                <i class="fa-solid fa-arrow-right"></i>
+                </button>
+                </div>
             </div>
         </div>
     </div>`
     })
 }
 
-loadData()
